8d5663ca6af9ec5dafba59b2a92e64173d8571ed
Akhtar <shadabakhar97@gmail.com>
2020-03-24 05:23:03 +0000

    file1.java compile and run successfully

--- file1.java
+++ file1.java
@@ -2,6 +2,6 @@ class file1
 {
   public static void main(String args[])
   {
-    System.out.println("We are working with git and github")
+    System.out.println("We are working with git and github");
   }
 } 
